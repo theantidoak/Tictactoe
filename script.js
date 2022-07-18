b570a5f7914448deb1bf77e0c01f8146a4e8ea27
theantidoak <tcdoak@gmail.com>
2022-07-18 11:56:05 -0400

    Restructure function checkWin and add x,o and reset buttons

--- script.js
+++ script.js
@@ -1,53 +1,72 @@
 const gameBoard = (() => {
-  const gameboardArray = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
 
   //cache DOM
   const gameSquares = document.querySelectorAll('.game-square');
+  const Xbutton = document.querySelector('.X-button');
+  const Obutton = document.querySelector('.O-button');
+  const resetButton = document.querySelector('.reset-button');
 
   //bind events
-  gameSquares.forEach(square => {
+  Xbutton.addEventListener('click', () => gameboardArray = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']);
+  Obutton.addEventListener('click', () => gameboardArray = ['O', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O']);
+  resetButton.addEventListener('click', beginGame);
+    
+  function beginGame() {
+    gameSquares.forEach((square) => {
+      while (square.firstChild) {
+        square.removeChild(square.lastChild);
+      }
+      gameboardArray = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
       square.addEventListener('click', placeMove);
-  });
+    })
+  }
 
   //render
   function placeMove() {
     const move = document.createTextNode(`${gameboardArray.pop()}`);
     this.appendChild(move);
     this.removeEventListener('click', placeMove);
-    checkWin();
+    _checkWin();
   }
 
-  function checkWin() {
-    if ((gameSquares[0].textContent == gameSquares[1].textContent && 
-      gameSquares[0].textContent == gameSquares[2].textContent && 
-      gameSquares[0].textContent !== '') ||
-    (gameSquares[0].textContent == gameSquares[3].textContent && 
-      gameSquares[0].textContent == gameSquares[6].textContent && 
-      gameSquares[0].textContent !== '') || 
-    (gameSquares[0].textContent == gameSquares[4].textContent && 
-      gameSquares[0].textContent == gameSquares[8].textContent && 
-      gameSquares[0].textContent !== '') ||
-    (gameSquares[1].textContent == gameSquares[4].textContent && 
-      gameSquares[1].textContent == gameSquares[7].textContent && 
-      gameSquares[1].textContent !== '') ||
-    (gameSquares[2].textContent == gameSquares[5].textContent && 
-      gameSquares[2].textContent == gameSquares[8].textContent && 
-      gameSquares[2].textContent !== '') ||
-    (gameSquares[2].textContent == gameSquares[4].textContent && 
-      gameSquares[2].textContent == gameSquares[6].textContent && 
-      gameSquares[2].textContent !== '') ||
-    (gameSquares[3].textContent == gameSquares[4].textContent && 
-      gameSquares[3].textContent == gameSquares[5].textContent && 
-      gameSquares[3].textContent !== '') ||
-    (gameSquares[6].textContent == gameSquares[7].textContent && 
-      gameSquares[6].textContent == gameSquares[8].textContent && 
-      gameSquares[6].textContent !== '')) {
-      console.log('Someone wins');
-    } else {
-      if (gameboardArray.length > 0) return;
-      console.log('Tie');
+  const _checkRows = () => {
+    for (let i = 0; i < 3; i++) {
+      let winningRow = [];
+      for (let j = i*3; j < i*3 + 3; j++) {
+        winningRow.push(gameSquares[j].textContent);
+      }
+      if (winningRow.every(field => field === 'X') || winningRow.every(field => field === 'O')) {
+        return true;
+      } 
     }
   }
 
-  return {placeMove, checkWin};
+  const _checkColumns = () => {
+    for (let i = 0; i < 3; i++) {
+      let winningColumn = [];
+      for (let j = i; j < i + 7; j+=3) {
+        winningColumn.push(gameSquares[j].textContent);
+      }
+      if (winningColumn.every(field => field === 'X') || winningColumn.every(field => field === 'O')) {
+        return true;
+      }
+    }
+  }
+
+  const _checkDiagonals = () => {
+    let firstDiagonal = [gameSquares[0].textContent, gameSquares[4].textContent, gameSquares[8].textContent]
+    let secondDiagonal = [gameSquares[2].textContent, gameSquares[4].textContent, gameSquares[6].textContent]
+    if (firstDiagonal.every(field => field === 'X') || secondDiagonal.every(field => field === 'X') ||
+      firstDiagonal.every(field => field == 'O') || secondDiagonal.every(field  => field == 'O')) {
+          return true;
+        } 
+  }
+
+  function _checkWin() {
+    if (_checkDiagonals() || _checkColumns() || _checkRows()) {
+      console.log('Win');
+    }
+  }
+
+  return {placeMove};
 })();
