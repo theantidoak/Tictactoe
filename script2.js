0192aeea62c006f1d07fa8f6f172f6c66930d76d
theantidoak <tcdoak@gmail.com>
2022-08-08 19:44:36 +0200

    Restart javascript file with new understanding of module patterns and factory functions

---
+++ script2.js
@@ -0,0 +1,87 @@
+const board = (() => {
+
+    const squares = [...document.querySelectorAll('.game-square')];
+
+
+    // const addtoArray;
+    return {squares};
+    
+})();
+
+const gameController = (() => {
+    let currentGame = board.squares.map((square) => square.textContent);
+    board.squares.forEach((square) => square.addEventListener('click', placeMove));
+
+    function render() {
+        const move = document.createTextNode('X');
+        this.appendChild(move);
+    }
+
+    function placeMove() {
+        if (_squareIsEmpty.call(this) == false) return;
+        render.call(this);
+        updateGame();
+        _checkWin();
+    }
+
+    const updateGame = function() {
+        currentGame = board.squares.map((square) => square.textContent);
+    };
+
+
+    const _squareIsEmpty = function() {
+        return this.textContent == "" ? true : false;
+    };
+
+    const _checkRows = () => {
+        for (let i = 0; i < 3; i++) {
+          const winningRow = [];
+          for (let j = i*3; j < i*3 + 3; j++) {
+            winningRow.push(currentGame[j]);
+          }
+          if (winningRow.every(field => field === 'X') || 
+            winningRow.every(field => field === 'O')) {
+            return true;
+          } 
+        }
+      }
+    
+      const _checkColumns = () => {
+        for (let i = 0; i < 3; i++) {
+          const winningColumn = [];
+          for (let j = i; j < i + 7; j+=3) {
+            winningColumn.push(currentGame[j]);
+          }
+          if (winningColumn.every(field => field === 'X') || 
+            winningColumn.every(field => field === 'O')) {
+            return true;
+          }
+        }
+      }
+    
+      const _checkDiagonals = () => {
+        console.log(currentGame);
+        const firstDiagonal = [currentGame[0], currentGame[4], currentGame[8]];
+        const secondDiagonal = [currentGame[0], currentGame[4], currentGame[8]];
+        if (firstDiagonal.every(field => field === 'X') || secondDiagonal.every(field => field === 'X') ||
+          firstDiagonal.every(field => field == 'O') || secondDiagonal.every(field  => field == 'O')) {
+              return true;
+            } 
+      }
+
+      function _checkWin() {
+        if (_checkDiagonals() || _checkColumns() || _checkRows()) {
+          console.log('You Win');
+        }
+        // } else if (!token) { // Come back here
+        //   displayResult.textContent = 'It\'s a tie';
+        // }
+      }
+
+    // const render;
+    // const checkForWin;
+    // const changePlayer;
+    
+    return {currentGame};
+
+})();
