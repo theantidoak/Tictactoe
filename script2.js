f0337adfee6181e4341506f653f50351f45e48db
theantidoak <tcdoak@gmail.com>
2022-08-09 13:57:52 +0200

    Add to player factory function and seperate checkWin function into different winners

--- script2.js
+++ script2.js
@@ -1,6 +1,11 @@
 const board = (() => {
 
+  //cache DOM
     const squares = [...document.querySelectorAll('.game-square')];
+    const player1Input = document.querySelector('#player-1');
+    const player2Input = document.querySelector('#player-2');
+    const resetButton = document.querySelector('.reset-button');
+
     const currentGame = squares.map((square) => square.textContent);
 
     const updateGame = function() {
@@ -8,25 +13,43 @@ const board = (() => {
         currentGame.splice(index, 1, this.textContent);
     };
 
-    return {squares, currentGame, updateGame};
+    const reset = function() {
+      squares.forEach((square) => {
+        while (square.firstChild) {
+          square.removeChild(square.lastChild);
+        }
+      });
+      currentGame.splice(0, currentGame.length, ...squares.map((square) => square.textContent));
+    }
+
+    return {squares, currentGame, updateGame, player1Input, player2Input, reset, resetButton};
     
 })();
 
-const gameController = (() => {
+const controller = (() => {
     const currentGame = board.currentGame;
+    const player1 = player(board.player1Input.value, 'X');
+    const player2 = player(board.player2Input.value, 'O');
 
+    //bind events
     board.squares.forEach((square) => square.addEventListener('click', placeMove));
-
-    function render() {
-        const move = document.createTextNode('X');
-        this.appendChild(move);
-    }
+    board.resetButton.addEventListener('click', board.reset);
 
     function placeMove() {
-        if (_squareIsEmpty.call(this) == false) return;
-        render.call(this);
-        board.updateGame.call(this);
-        _checkWin();
+      if (_squareIsEmpty.call(this) == false) return;
+      render.call(this);
+      board.updateGame.call(this);
+      _checkWin();
+      _checkGameOver();
+    }
+
+    const changePlayer = function() {
+      return player1.takeTurn().turn ? 'X' : 'O';
+    }
+
+    const render = function() {
+        const move = document.createTextNode(changePlayer());
+        this.appendChild(move);
     }
 
     const _squareIsEmpty = function() {
@@ -39,44 +62,77 @@ const gameController = (() => {
           for (let j = i*3; j < i*3 + 3; j++) {
             winningRow.push(currentGame[j]);
           }
-          if (winningRow.every(field => field === 'X') || 
-            winningRow.every(field => field === 'O')) {
-            return true;
+          if (winningRow.every(field => field === 'X')) { 
+            return 'xWins';
+          } else if (winningRow.every(field => field === 'O')) {
+            return 'oWins';
           } 
         }
       }
     
-      const _checkColumns = () => {
-        for (let i = 0; i < 3; i++) {
-          const winningColumn = [];
-          for (let j = i; j < i + 7; j+=3) {
-            winningColumn.push(currentGame[j]);
-          }
-          if (winningColumn.every(field => field === 'X') || 
-            winningColumn.every(field => field === 'O')) {
-            return true;
-          }
+    const _checkColumns = () => {
+      for (let i = 0; i < 3; i++) {
+        const winningColumn = [];
+        for (let j = i; j < i + 7; j+=3) {
+          winningColumn.push(currentGame[j]);
         }
+        if (winningColumn.every(field => field === 'X')) {
+          return 'xWins';
+        } else if (winningColumn.every(field => field === 'O')) {
+          return 'oWins';
+        } 
       }
+    }
     
-      const _checkDiagonals = () => {
-        console.log(currentGame);
-        const firstDiagonal = [currentGame[0], currentGame[4], currentGame[8]];
-        const secondDiagonal = [currentGame[0], currentGame[4], currentGame[8]];
-        if (firstDiagonal.every(field => field === 'X') || secondDiagonal.every(field => field === 'X') ||
-          firstDiagonal.every(field => field == 'O') || secondDiagonal.every(field  => field == 'O')) {
-              return true;
-            } 
-      }
+    const _checkDiagonals = () => {
+      console.log(currentGame);
+      const firstDiagonal = [currentGame[0], currentGame[4], currentGame[8]];
+      const secondDiagonal = [currentGame[2], currentGame[4], currentGame[6]];
+      if (firstDiagonal.every(field => field === 'X') || secondDiagonal.every(field => field === 'X')) {
+        return 'xWins';
+      } else if (firstDiagonal.every(field => field == 'O') || secondDiagonal.every(field  => field == 'O')) {
+        return 'oWins'
+      } 
+    }
 
-      function _checkWin() {
-        if (_checkDiagonals() || _checkColumns() || _checkRows()) {
-          console.log('You Win');
-        }
+    function _checkWin() {
+      const diagonals = _checkDiagonals();
+      const columns = _checkColumns();
+      const rows = _checkRows();
+      if (diagonals == 'xWins' || columns == 'xWins' || rows == 'xWins') {
+        ++player1.wins;
+        console.log('xWins');
+      } else if (diagonals == 'oWins' || columns == 'oWins' || rows == 'oWins') {
+        ++player2.wins;
+        console.log('oWins');
       }
+    }
 
-    // const changePlayer;
-    
-    return {currentGame};
+    function _checkGameOver() {
+      if (player1.wins == 3) {
+        console.log('Player 1 wins the game');
+      }
+      else if (player2.wins == 3) {
+        console.log('Player 2 wins the game');
+      }
+    }
+
+    return {player1};
 
 })();
+
+function player(name, token) {
+  
+  let turn = token == 'X' ? false : true;
+  let wins = 0;
+  //choose 1P or 2P
+  //have an opponent
+
+  // const getToken = () => getToken;
+  // const getName = () => name;
+  const takeTurn = () => {
+    turn = !turn;
+    return {turn};
+  };
+  return {takeTurn, wins};
+}
