c681f34108bd03ca0c48860afbc7b47231cd0b07
theantidoak <tcdoak@gmail.com>
2022-08-10 16:50:24 +0200

    Modify the choose sides section to update the score

--- script2.js
+++ script2.js
@@ -1,84 +1,105 @@
+const dragAndDrop = (() => {
+  //cache DOM
+  const playerOptions = document.querySelectorAll('.draggables .positioned');
+  const dropbox1 = document.querySelector('#drop1');
+  const dropbox2 = document.querySelector('#drop2');
+
+  //bind events
+  playerOptions.forEach((option) => option.addEventListener('dragstart', drag));
+  playerOptions.forEach((option) => option.parentElement.addEventListener('dragover', allowDrop));
+  playerOptions.forEach((option) => option.parentElement.addEventListener('drop', drop));
+  dropbox1.addEventListener('dragstart', drag);
+  dropbox1.addEventListener('dragover', allowDrop);
+  dropbox1.addEventListener('drop', drop);
+  dropbox2.addEventListener('dragstart', drag);
+  dropbox2.addEventListener('dragover', allowDrop);
+  dropbox2.addEventListener('drop', drop);
+
+  function drag(e) {
+    e.dataTransfer.setData("text/plain", e.target.id);
+  }
+
+  function allowDrop(e) {
+    e.preventDefault();
+  }
+
+  function drop(e) { 
+    if (checkIfEmpty(this) == false) return;
+    const data = e.dataTransfer.getData("text/plain");
+    const draggable = document.getElementById(data);
+    this.appendChild(draggable);
+    modifyContent.call(this, data);
+  }
+
+  const modifyContent = function(that) {
+    const target = document.getElementById(that);
+    const dropBox = this;
+    switch (dropBox.id) {
+      case 'drop1':
+        target.textContent = that[0] == 'h' ? 'P1 : 0' : 'B1 : 0';
+        break;
+      case 'drop2':
+        target.textContent = that[0] == 'h' ? 'P2 : 0' : 'B2 : 0';
+        break;
+      default:
+        target.textContent = that[0] == 'h' ? 
+          `\u{2261} Player` : 
+          `\u{2261} Bot`;
+    }
+  }
+
+  const checkIfEmpty = function(that) {
+    if (that.classList.contains('dropbox') && that.children.length == 1) {
+      return false;
+    }
+  }
+  return {dropbox1, dropbox2, playerOptions};
+
+})();
+
 const board = (() => {
 
   //cache DOM
-    const squares = [...document.querySelectorAll('.game-square')];
-    const player1Input = document.querySelector('#player-1');
-    const player2Input = document.querySelector('#player-2');
-    const resetButton = document.querySelector('.reset-button');
-    const playerOptions = document.querySelectorAll('.draggables div');
-    const dropbox1 = document.querySelector('#drop1');
-    const dropbox2 = document.querySelector('#drop2');
+  const squares = [...document.querySelectorAll('.game-square')];
+  const player1Input = document.querySelector('#player-1');
+  const player2Input = document.querySelector('#player-2');
+  const resetButton = document.querySelector('.reset-button');
+  
+  const currentGame = squares.map((square) => square.textContent);
 
-    const currentGame = squares.map((square) => square.textContent);
+  const updateBoard = function() {
+      const index = squares.indexOf(this);
+      currentGame.splice(index, 1, this.textContent);
+  };
 
-    // playerOptions.forEach((button) => button.addEventListener('click', selectSide));
-    playerOptions.forEach((option) => option.addEventListener('dragstart', drag));
-    dropbox1.addEventListener('dragover', allowDrop);
-    dropbox1.addEventListener('drop', drop);
-    dropbox2.addEventListener('dragover', allowDrop);
-    dropbox2.addEventListener('drop', drop);
+  const updateScore = function() {
+    dragAndDrop.playerOptions.forEach((option) => {
+      if (option.textContent[0] == `\u{2261}`) return;
+      const content = option.textContent;
+      option.textContent = option.parentElement.id == 'drop1' ?
+       content.substring(0, content.length-1) + controller.player1.wins :
+       content.substring(0, content.length-1) + controller.player2.wins
+    })
+  }
 
-    function drag(e) {
-      e.dataTransfer.setData("text/plain", e.target.id);
-    }
+  const reset = function() {
+    squares.forEach((square) => {
+      while (square.firstChild) {
+        square.removeChild(square.lastChild);
+      }
+    });
+    currentGame.splice(0, currentGame.length, ...squares.map((square) => square.textContent));
+  }
 
-    function allowDrop(e) {
-      e.preventDefault();
-    }
-
-    function drop(e) {
-      e.preventDefault();
-      const data = e.dataTransfer.getData("text/plain");
-      const draggable = document.getElementById(data);
-      e.target.appendChild(draggable);
-      removeAbsolutePostition(draggable);
-      e.target == dropbox1 ? 
-        dropbox1.removeEventListener('drop', drop) : 
-        dropbox2.removeEventListener('drop', drop);
-    }
-
-    const removeAbsolutePostition = (draggable) => {
-      draggable.style.position = 'static';
-    }
-
-    // function selectSide(e) {
-    //   e.preventDefault();
-    //   console.log(e.target);
-    //   switch (this.getAttribute('class')) {
-    //     case 'human1':
-    //       break;
-    //     case 'bot1':
-    //       break;
-    //     case 'human2':
-    //       break;
-    //     case 'bot2':
-    //       break;
-    //   }
-    // }
-
-    const updateGame = function() {
-        const index = squares.indexOf(this);
-        currentGame.splice(index, 1, this.textContent);
-    };
-
-    const reset = function() {
-      squares.forEach((square) => {
-        while (square.firstChild) {
-          square.removeChild(square.lastChild);
-        }
-      });
-      
-      currentGame.splice(0, currentGame.length, ...squares.map((square) => square.textContent));
-    }
-
-    return {squares, currentGame, updateGame, player1Input, player2Input, reset, resetButton};
-    
+  return {squares, currentGame, updateBoard, updateScore, player1Input, player2Input, reset, resetButton};
+  
 })();
 
 const controller = (() => {
     const currentGame = board.currentGame;
-    const player1 = player('X');
-    const player2 = player('O');
+    const player1 = player(dragAndDrop.dropbox1.getAttribute('name'));
+    const player2 = player(dragAndDrop.dropbox2.getAttribute('name'));
+    
 
     //bind events
     board.squares.forEach((square) => square.addEventListener('click', placeMove));
@@ -87,8 +108,9 @@ const controller = (() => {
     function placeMove() {
       if (_squareIsEmpty.call(this) == false) return;
       render.call(this);
-      board.updateGame.call(this);
+      board.updateBoard.call(this);
       _checkWin();
+      board.updateScore();
       _checkGameOver();
     }
 
@@ -156,12 +178,10 @@ const controller = (() => {
         ++player1.wins;
         player1.takeTurn();
         setTimeout(board.reset, 1000);
-        return 'win';
       } else if (diagonals == 'oWins' || columns == 'oWins' || rows == 'oWins') {
         ++player2.wins;
         player2.takeTurn();
         setTimeout(board.reset, 1000);
-        return 'win';
       }
     }
 
@@ -173,20 +193,18 @@ const controller = (() => {
         console.log('Player 2 wins the game');
       }
     }
-    return {player1};
-
+    return {player1, player2};
 })();
 
+
 function player(token) {
-  
+  this.token = token;
   let turn = token == 'X' ? false : true;
   let wins = 0;
-  //choose 1P or 2P
-  //have an opponent
 
   const takeTurn = () => {
     turn = !turn;
     return {turn};
   };
-  return {takeTurn, wins};
+  return {takeTurn, wins, token};
 }
